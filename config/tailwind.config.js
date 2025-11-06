8bd70bf725559b2f05d33a76fa2f0d6d1713d6b8
Juan Manuel Mougan <juanmougan@gmail.com>
2025-11-06 21:10:09 +0100

    feat: started with rodauth views

---
+++ config/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
